3dd9c26671bef5a5b2353ca5c06b20fcad6a80c3
Thomas Le Bihan <thomas.66days@gmail.com>
2023-08-08 10:33:37 +0200

    remover terser plugin in rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,10 +2,7 @@ import packageJson from "./package.json" assert { type: "json" };
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import terser from "@rollup/plugin-terser";
 import dts from "rollup-plugin-dts";
-
-// const packageJson = require("./package.json");
 
 export default [
   {
@@ -31,6 +28,6 @@ export default [
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts(), terser()],
+    plugins: [dts()],
   },
 ];
